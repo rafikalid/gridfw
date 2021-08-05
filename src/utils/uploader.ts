e40bfbd1c29e7c4c896434dbe2cf88a2df522511
khalid RAFIK <khalid.rfk@gmail.com>
2021-08-06 00:24:22 +0100

    cc

--- src/utils/uploader.ts
+++ src/utils/uploader.ts
@@ -1,21 +1,20 @@
-import type { Context } from "@src/context";
 import { ErrorCodes, GError } from "@src/error";
+import {Request} from '../http/request';
 
 /** Uploader options */
 export interface UploaderOptions{
-
+	limits?: 
 }
 
 /**
  * Make upload
  */
-export async function upload(ctx: Context<any, any>, options: UploaderOptions= {}): Promise<any>{
+export async function upload(req: Request<any, any>, options: UploaderOptions= {}): Promise<any>{
 	//* Prepare
-	const req = ctx.req;
-	const app= ctx.app;
-	const contentType = ctx.reqContentType?.type;
+	const app= req.app;
+	const contentType = req.contentType?.type;
 	if(contentType==null) throw new GError(ErrorCodes.UPLOAD_ERROR, "Missing data content type!");
 	//* Limits
-	const limits= options.limits==null ? app.uploadLimits
+	const limits= options.limits ? app.uploadLimits
 }
 
